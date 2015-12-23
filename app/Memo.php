460d65b73af3f38d3a483fa98d20ba3768779e9f
niiyz <yoshida@niiyz.com>
2015-12-23 17:23:13 +0900

    Add Memo Model fill prop array.

--- app/Memo.php
+++ app/Memo.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Memo extends Model
 {
+    protected $fillable = ['text'];
+
     public function scopeLast1($query)
     {
         return $query->orderBy('id', 'desc')->first();
